da7ebf556b7a034800987eb95fdafd95b91e58ee
novice <619671969@qq.com>
2024-06-24 17:48:13 +0800

    lab almost finished!

--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-    int main (){
-    int a;
-    a = 1;
-    printf("%d\n",a);
-    printf("I'm happy to learn vim!");
-    return 0;
-}
--- testnvim.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a = 1;
-    int b = 2;
-    printf("the number of a is : %d\n,the number of b is %d\n",a,b);
-    return 0;
-} 
